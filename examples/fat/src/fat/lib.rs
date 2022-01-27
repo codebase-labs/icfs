a53dc35b8201ee1e2b026d9ef77ae084b3f08882
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-01-27 00:06:52 -0800

    [WIP]

--- examples/fat/src/fat/lib.rs
+++ examples/fat/src/fat/lib.rs
@@ -1,5 +1,6 @@
 use ic_cdk_macros::{init, update};
 use std::io::Write;
+use std::io::Seek;
 
 thread_local! {
     static FS: std::cell::RefCell<fscommon::BufStream<icfs::StableMemory>>
@@ -13,19 +14,30 @@ fn init() {
 
 fn _init() -> Result<(), std::io::Error> {
     // TODO: FS.with(|fs| )
-    let mut fs = icfs::StableMemory::default();
+    let mut stable_memory = icfs::StableMemory::default();
 
     // A Wasm memory page is 2^16 bytes. Canisters have a 4 Gigabyte limit. 4 GB
     // is 2^16 * 2^16 bytes. Apparently we can grow beyond that to 2^17 pages.
-    fs.grow(2^17)?;
+    stable_memory.grow(2^17)?;
 
-    let fs = fscommon::BufStream::new(fs);
+    let stable_memory = fscommon::BufStream::new(stable_memory);
 
     fatfs::format_volume(
-        &mut fatfs::StdIoWrapper::from(fs),
-        fatfs::FormatVolumeOptions::new().fats(1),
+        &mut fatfs::StdIoWrapper::from(stable_memory),
+        fatfs::FormatVolumeOptions::new(),
     )
-    .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))
+    .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+
+    // TEMP
+    let options = fatfs::FsOptions::new().update_accessed_date(true);
+    let fs = fatfs::FileSystem::new(stable_memory, options)?;
+
+    let name = "World";
+
+    let root_dir = fs.root_dir();
+    let mut file = root_dir.create_file("hello.txt")?;
+    let contents = format!("Hello, {}!", name).into_bytes();
+    file.write_all(&contents)
 }
 
 #[update]
