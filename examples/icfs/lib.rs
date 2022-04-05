eb658371034574c74161a527ab220c1edc6e3846
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-04-05 13:23:48 -0700

    Add writer seek tests

--- examples/icfs/lib.rs
+++ examples/icfs/lib.rs
@@ -5,7 +5,7 @@
 #![feature(write_all_vectored)]
 
 use ic_cdk_macros::{init, query, update};
-use std::io::{IoSlice, Read, Seek, Write};
+use std::io::{IoSlice, IoSliceMut, Read, Seek, SeekFrom, Write};
 
 thread_local! {
     static STABLE_MEMORY: std::cell::RefCell<icfs::StableMemory>
@@ -30,5 +30,42 @@ fn test_writer() {
         assert_eq!(&icfs::StableMemory::bytes()[0..11], b);
 
         ic_cdk::api::stable::stable64_write(0, &vec![0; 11]);
+    })
+}
+
+#[update]
+fn test_writer_seek() {
+    STABLE_MEMORY.with(|stable_memory| {
+        let mut writer = *stable_memory.borrow();
+
+        assert_eq!(writer.stream_position().unwrap(), 0);
+        assert_eq!(writer.write(&[1]).unwrap(), 1);
+        assert_eq!(writer.stream_position().unwrap(), 1);
+
+        assert_eq!(writer.seek(SeekFrom::Start(2)).unwrap(), 2);
+        assert_eq!(writer.stream_position().unwrap(), 2);
+        assert_eq!(writer.write(&[2]).unwrap(), 1);
+        assert_eq!(writer.stream_position().unwrap(), 3);
+
+        assert_eq!(writer.seek(SeekFrom::Current(-2)).unwrap(), 1);
+        assert_eq!(writer.stream_position().unwrap(), 1);
+        assert_eq!(writer.write(&[3]).unwrap(), 1);
+        assert_eq!(writer.stream_position().unwrap(), 2);
+
+        let capacity = icfs::StableMemory::capacity();
+
+        assert_eq!(writer.seek(SeekFrom::End(-1)).unwrap(), capacity as u64 - 1);
+        assert_eq!(writer.stream_position().unwrap(), capacity as u64 - 1);
+        assert_eq!(writer.write(&[4]).unwrap(), 1);
+        assert_eq!(writer.stream_position().unwrap(), capacity as u64);
+
+        let b: &[_] = &[1, 3, 2, 0, 0, 0, 0, 0];
+        assert_eq!(&icfs::StableMemory::bytes()[0..8], b);
+
+        let b: &[_] = &[0, 0, 0, 0, 0, 0, 0, 4];
+        assert_eq!(&icfs::StableMemory::bytes()[(capacity - 8)..], b);
+
+        ic_cdk::api::stable::stable64_write(0, &vec![0; 8]);
+        ic_cdk::api::stable::stable64_write(capacity as u64 - 8, &vec![0; 8]);
     })
 }
