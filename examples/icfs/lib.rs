bf363ddff774252932b5c6c781c8eb93910c5442
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-04-05 15:02:22 -0700

    Add reader tests

--- examples/icfs/lib.rs
+++ examples/icfs/lib.rs
@@ -69,3 +69,43 @@ fn test_writer_seek() {
         ic_cdk::api::stable::stable64_write(capacity as u64 - 8, &vec![0; 8]);
     })
 }
+
+#[update]
+fn test_reader() {
+    STABLE_MEMORY.with(|stable_memory| {
+        let mut stable_memory = *stable_memory.borrow();
+        stable_memory.write(&[0, 1, 2, 3, 4, 5, 6, 7]).unwrap();
+        stable_memory.seek(SeekFrom::Start(0)).unwrap();
+
+        let mut buf = [];
+        assert_eq!(stable_memory.read(&mut buf).unwrap(), 0);
+        assert_eq!(stable_memory.stream_position().unwrap(), 0);
+
+        let mut buf = [0];
+        assert_eq!(stable_memory.read(&mut buf).unwrap(), 1);
+        assert_eq!(stable_memory.stream_position().unwrap(), 1);
+
+        let b: &[_] = &[0];
+        assert_eq!(buf, b);
+
+        let mut buf = [0; 4];
+        assert_eq!(stable_memory.read(&mut buf).unwrap(), 4);
+        assert_eq!(stable_memory.stream_position().unwrap(), 5);
+
+        let b: &[_] = &[1, 2, 3, 4];
+        assert_eq!(buf, b);
+        assert_eq!(stable_memory.read(&mut buf).unwrap(), 4);
+
+        let b: &[_] = &[5, 6, 7, 0];
+        assert_eq!(buf, b);
+
+        let b: &[_] = &[5, 6, 7];
+        assert_eq!(&buf[..3], b);
+
+        assert_eq!(stable_memory.read(&mut buf).unwrap(), 4);
+        let b: &[_] = &[0, 0, 0, 0];
+        assert_eq!(buf, b);
+
+        ic_cdk::api::stable::stable64_write(0, &vec![0; 8]);
+    })
+}
