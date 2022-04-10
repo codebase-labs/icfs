aaf9ac7f7fe9a5a0979227a7e4f5b06b5f1eea8b
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-04-09 19:31:47 -0700

    Rename write_dir, add mkdir, add rm, support paths

--- examples/fatfs/lib.rs
+++ examples/fatfs/lib.rs
@@ -15,6 +15,13 @@ type FileSystem = fatfs::FileSystem<
     fatfs::LossyOemCpConverter,
 >;
 
+type Dir<'a> = fatfs::Dir<
+    'a,
+    fatfs::StdIoWrapper<icfs::StableMemory>,
+    icfs_fatfs::TimeProvider,
+    fatfs::LossyOemCpConverter,
+>;
+
 thread_local! {
     // static STABLE_MEMORY: std::cell::RefCell<fscommon::BufStream<icfs::StableMemory>>
     //     = std::cell::RefCell::new(fscommon::BufStream::new(icfs::StableMemory::default()));
@@ -56,30 +63,48 @@ thread_local! {
     };
 }
 
-#[query]
-fn ls() -> Vec<String> {
-    FS.with(|fs| {
-        let fs = fs.borrow();
-        let root_dir = fs.root_dir();
-        let entries: std::io::Result<Vec<String>> = root_dir
-            .iter()
-            .map(|entry| {
-                entry
-                    .map(|e| e.file_name())
-                    .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))
-            })
-            .collect();
-        entries
-    })
-    .unwrap()
+fn open_dir_path<'a>(fs: &'a FileSystem, path: &str) -> std::io::Result<Dir<'a>> {
+    let root_dir = fs.root_dir();
+    let (base_dir_name, sub_dir_path) = path_head_tail(&path)
+        .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+    match (base_dir_name.as_str(), sub_dir_path.as_str()) {
+        (".", "") => Ok(root_dir),
+        (".", sub_dir_path) => root_dir
+            .open_dir(&sub_dir_path)
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error)),
+        _ => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Invalid path: {}", path.to_string()),
+        )),
+    }
+}
+
+fn path_head_tail(path: &str) -> Result<(String, String), String> {
+    let path_segments = path.split("/").collect::<Vec<_>>();
+    let head = path_segments
+        .first()
+        .ok_or(format!("Invalid path: {}", path.to_string()))?;
+    let tail = path_segments[1..].join("/");
+    Ok((head.to_string(), tail))
+}
+
+fn path_init_last(path: &str) -> Result<(String, String), String> {
+    let mut path_segments = path.split("/").collect::<Vec<_>>();
+    let last = path_segments
+        .pop()
+        .ok_or(format!("Invalid path: {}", path.to_string()))?;
+    let init = path_segments.join("/");
+    Ok((init, last.to_string()))
 }
 
 #[query]
-fn read_file(filename: String) -> String {
+fn cat(path: String) -> String {
     FS.with(|fs| {
         let fs = fs.borrow();
-        let root_dir = fs.root_dir();
-        let mut file = root_dir.open_file(&filename)?;
+        let (dir_path, file_name) = path_init_last(&path)
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+        let dir = open_dir_path(&fs, &dir_path)?;
+        let mut file = dir.open_file(&file_name)?;
         let mut buf = vec![];
         file.read_to_end(&mut buf)?;
         let contents = String::from_utf8(buf)
@@ -89,12 +114,64 @@ fn read_file(filename: String) -> String {
     .unwrap()
 }
 
-#[update]
-fn write_file(filename: String, contents: String) {
+#[query]
+fn ls(path: String) -> Vec<String> {
     FS.with(|fs| {
         let fs = fs.borrow();
-        let root_dir = fs.root_dir();
-        let mut file = root_dir.create_file(&filename)?;
+        let dir = open_dir_path(&fs, &path)?;
+        let mut entries: std::io::Result<Vec<String>> = dir
+            .iter()
+            .map(|entry| {
+                entry
+                    .map(|e| e.file_name())
+                    .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))
+            })
+            .collect();
+
+        match entries.as_mut() {
+            Ok(ok) => ok.sort(),
+            Err(_err) => (),
+        }
+
+        entries
+    })
+    .unwrap()
+}
+
+#[update]
+fn mkdir(path: String) {
+    FS.with(|fs| {
+        let fs = fs.borrow();
+        let (dir_path, dir_name) = path_init_last(&path)
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+        let dir = open_dir_path(&fs, &dir_path)?;
+        dir.create_dir(&dir_name)?;
+        std::io::Result::Ok(())
+    })
+        .unwrap()
+}
+
+#[update]
+fn rm(path: String) {
+    FS.with(|fs| {
+        let fs = fs.borrow();
+        let (dir_path, target) = path_init_last(&path)
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+        let dir = open_dir_path(&fs, &dir_path)?;
+        dir.remove(&target)?;
+        std::io::Result::Ok(())
+    })
+    .unwrap()
+}
+
+#[update]
+fn write_file(path: String, contents: String) {
+    FS.with(|fs| {
+        let fs = fs.borrow();
+        let (dir_path, file_name) = path_init_last(&path)
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+        let dir = open_dir_path(&fs, &dir_path)?;
+        let mut file = dir.create_file(&file_name)?;
         file.truncate()?;
         file.write_all(&contents.into_bytes())?;
         file.flush()?;
